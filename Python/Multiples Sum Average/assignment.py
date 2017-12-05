b586eb3b821e64d00796a70710bd1bce7914423d
Charlie Hall <charlie@halldom.net>
2017-12-05 16:01:18 -0500

    Multiples Assignment

---
+++ Python/Multiples Sum Average/assignment.py
@@ -0,0 +1,17 @@
+#multiples A
+for count in range(1, 1001, 2):
+    print count
+
+#multiples B
+for count in range (5, 1000000, 5):
+    print count
+
+#sumlist
+a = [1, 2, 5, 10, 255, 3]
+sum = 0
+for i in a:
+    sum += i
+print sum
+
+#averagevalues
+print sum/len(a)
